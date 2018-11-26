da27d8eb672e06e2a34eae7f7d4eb0e0b97b9b6d
Samuel Yeomans <samuelyeomans@gmail.com>
2018-11-26 15:35:05 -0500

    This code was awful. Scrapped and rewritten.
    Code is now an independent function that runs more smoothly. Input is a string containing a die roll formula. Returns two results: the integer total rolled and the list of dice (integers) that returned that result. 
    
    Note: Requires random library

--- Dice.py
+++ Dice.py
@@ -1,99 +1,40 @@
-#Import libraries and begin while loop
-from random import *
+import random
 
-#Loop until user enters a blank input string
-running = True
-while running:
-	#Get text input from user. If blank, stop while loop
-	print("\n")
-	inStr = "0+" #String evaluation begins at 0
-	inStr += str (raw_input  ("Enter a die roll (ex: d20adv+2d4-1): "))
-	#If user entered a blank string, break and end script.
-	if inStr == "0+":
-		running = False
-		break
+def roll(inStr):
+	# Split input string into a list of die roll formulas
+		# example inStr: '1d20+1d4+3'
+		# example inList: ['1d20', '1d4', '3']
+	inStr = inStr.replace('-', '+-') # Account for negative bonuses and rolls
+	inList = inStr.split("+") # Split into list of [x]d[y] formulas
 
-	#Format die roll. Put a delimiting space between every die or bonus and separate into a list.
-	inStr = inStr.replace ("+"," +")
-	inStr = inStr.replace ("-"," -")
-	inStr = inStr.replace ("+d","+1d")
-	inStr = inStr.replace ("-d","-1d")
-	inStr = inStr.replace ("adv","x") #mark advantage rolls with an x (for ease of use)
-	inStr = inStr.replace ("dis","y") #mark disadvantage rolls with a y (for ease of use)
-	#Convert input string to a list of dice and bonuses
-	inList = inStr.split (" ")
-	#example looks like ['0', '+1d20x', '+2d4', '-1']
+	# Initialize return variables
+	resultList = []
 
-	#Roll each die and add to roll string. Bonus is tallied separately to make output easier to read.
-	bonus = 0
-	rollStr = ""
-	prntStr = ""
-	for die in inList:
-		#Detect if list element is a die. Else, it's a bonus.
-		if "d" in die:
-			#If die is marked with advantage 
-			if "x" in die:
-				#Preserve the sign (positive or negative) and move to its own variable
-				sign = die[0]
-				die = die[1:]
-				#Remove advantage mark
-				die = die.replace("x","")
-				#Number before "d" is the number of dice to roll; number after is the value of the die.
-				#Roll a [after "d"] die [before "d"] times.
-				roll = die.split("d")
-				for i in range(0,int(roll[0])):
-					#Roll this die twice and take the lower value. Add result to roll string.
-					compare = [0,0]
-					compare[0] = int(randrange(1,1+int(roll[1])))
-					compare[1] = int(randrange(1,1+int(roll[1])))
-					rollStr += sign + str(max(compare))
-					prntStr += sign + str(compare)
-			#If die is marked with disadvantage
-			elif "y" in die:
-				#Preserve the sign (positive or negative) and move to its own variable
-				sign = die[0]
-				die = die[1:]
-				#Remove disadvantage mark
-				die = die.replace("y","")
-				#Number before "d" is the number of dice to roll; number after is the value of the die.
-				#Roll a [after "d"] die [before "d"] times.
-				roll = die.split("d")
-				for i in range(0,int(roll[0])):
-					#Roll this die twice and take the lower value. Add result to roll string.
-					compare = [0,0]
-					compare[0] = int(randrange(1,1+int(roll[1])))
-					compare[1] = int(randrange(1,1+int(roll[1])))
-					rollStr += sign + str(min(compare))
-					prntStr += sign + str(compare)
-			#Die is marked with neither advantage nor disadvantage
+	# Main loop: iterate through each individual [x]d[y] formula
+	for formula in inList:
+
+		# If this formula is a die roll formula (not a bonus), roll die formula
+		if 'd' in formula:
+			numDice, numSides = map(int, formula.split('d'))
+
+			# If this formula is negative, result should be negative
+			if '-' in formula:
+				# Roll [numDice] number of [numSides] sided dice and append to return list
+				numDice = numDice * -1
+				for i in range(0, numDice):
+					result = random.randrange(1, numSides+1) * -1
+					resultList.append(result)
+
+			# If the formula is not negative, continue as normal
 			else:
-				#Preserve the sign (positive or negative) and move to its own variable
-				sign = die[0]
-				die = die[1:]
-				#Number before "d" is the number of dice to roll; number after is the value of the die.
-				#Roll a [after "d"] die [before "d"] times.
-				roll = die.split("d")
-				for i in range(0,int(roll[0])):
-					val = sign + str(randrange(1,1+int(roll[1])))
-					rollStr += val
-					prntStr += val
-		#This element of inList is not a die; therefore it is a bonus. Tally separately for ease of reading.
+				# Roll [numDice] number of [numSides] sided dice and append to return list
+				for i in range(0, numDice):
+					result = random.randrange(1, numSides+1)
+					resultList.append(result)
+
+		# If this formula is a bonus, append to result
 		else:
-			bonus += int(die)
+			resultList.append(int(formula))
 
-	#If bonus is positive, add a "+" and add to roll string.
-	#If bonus is negative, it already has a "-"; just add to roll string.
-	#If bonus is 0, don't bother adding it to the roll string.
-	if bonus > 0:
-		rollStr += "+" + str(bonus)
-		prntStr += "+" + str(bonus)
-	elif bonus < 0:
-		rollStr += str(bonus)
-		prntStr += str(bonus)
-
-	#Evaluate roll string and print result
-	#prntStr is the same as rollStr, but prntStr shows both rolls on advantage/disadvantage
-	print(prntStr[1:]) #string always starts with unnecessary "+".
-	print "Total: " + str(eval(rollStr))
-
-	#Loop again
+	total = sum(resultList)
+	return(total, resultList)
